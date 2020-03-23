9fb4dcf84fb1021e7cd366f2b123ddd8c806a157
Zeyad Sabbah <55757830+ZeyadSabbah@users.noreply.github.com>
2020-03-23 11:50:37 -0400

    updated data_transformation class

--- data_transformation.py
+++ data_transformation.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from numpy import *
+import numpy as np
 from datetime import datetime
 from datetime import timedelta
 import math
@@ -19,6 +19,16 @@ class data_transformation:
               item_id = item_id.rename(columns={'impressions':'item_id'})
               item_id = item_id.reset_index(drop=True)
               return item_id
+              
+            def get_clickout(data_clickout, data, item_id):
+              clickout = data_clickout[['session_id','reference']]
+              clickout = item_id.merge(clickout, on='session_id', how='left')
+              clickout['clickout'] = clickout.apply(lambda x: 1 if x['item_id'] == x['reference'] else 0, axis=1)
+              clickout.drop(columns='reference', inplace=True)
+              clickout = clickout.reset_index(drop=True)
+              IndexToNan = data_clickout.reference.index.values.tolist()
+              data.loc[IndexToNan, 'reference'] = np.nan
+              return clickout, data
 
             def get_price(data_clickout):
               price = data_clickout[['session_id', 'prices']]
@@ -40,19 +50,11 @@ class data_transformation:
 
             def get_price_rank(data):
               price_rank = data.groupby('session_id', sort=False).price.apply(lambda x: x.values).to_frame().reset_index().rename(columns={'price':'price_list'})
-              price_rank.price_list = price_rank.price_list.apply(lambda x: argsort(x))
+              price_rank.price_list = price_rank.price_list.apply(lambda x: np.argsort(x))
               price_rank = price_rank.rename(columns={'price_list':'price_rank'})
               price_rank = price_rank.explode('price_rank')
               price_rank = price_rank.reset_index(drop=True)
               return price_rank
-
-            def get_clickout(data_clickout, item_id):
-              clickout = data_clickout[['session_id','reference']]
-              clickout = item_id.merge(clickout, on='session_id', how='left')
-              clickout['clickout'] = clickout.apply(lambda x: 1 if x['item_id'] == x['reference'] else 0, axis=1)
-              clickout.drop(columns='reference', inplace=True)
-              clickout = clickout.reset_index(drop=True)
-              return clickout
 
             def get_session_duration(data, item_id):
               session_duration = data.groupby('session_id', sort=False).timestamp.max() - data.groupby('session_id', sort=False).timestamp.min()
@@ -102,10 +104,10 @@ class data_transformation:
             
             FinalClickoutDF = get_data_clickout(data)
             item_id = get_item_id(FinalClickoutDF)
+            clickout, data = get_clickout(FinalClickoutDF, data, item_id)
             price = get_price(FinalClickoutDF)
             item_rank = get_item_rank(FinalClickoutDF)
             price_rank = get_price_rank(price)
-            clickout = get_clickout(FinalClickoutDF, item_id)
             session_duration = get_session_duration(data, item_id)
             item_duration = get_item_duration(data, item_id)
             item_session_duration = get_item_session_duration(item_duration, session_duration)
@@ -124,9 +126,15 @@ class data_transformation:
             local_data['item_interactions'] = item_interactions.item_interactions
             local_data['maximum_step'] = maximum_step.maximum_step
             local_data['top_list'] = top_list.top_list
-            GlobalPath = '/content/drive/My Drive/Trivago/Datasets/clean_data/item_global.csv'
+            GlobalPath = './Datasets/clean_data/ItemsFeatures/item_global.csv'
             GlobalData = pd.read_csv(GlobalPath)
             GlobalData.item_id = GlobalData.item_id.apply(lambda x: str(x))
             data = local_data.merge(GlobalData, on='item_id', how='left')
+            NaNcolumns = ['NumberOfProperties', 'NumberInImpressions', 'NumberInReferences', 'NumberAsClickout', 'NumberAsFinalClickout',
+                          'FClickoutToImpressions', 'FClickoutToReferences', 'FClickoutToClickout', 'MeanPrice', 'AveragePriceRank']
+            for column in NaNcolumns:
+              MeanValue = data[column].mean()
+              data[column] =  data[column].fillna(MeanValue)
+            data.item_id = data.item_id.apply(lambda x: str(x))
 
             return data
